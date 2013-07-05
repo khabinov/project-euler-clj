17ced507ed520d948374142b7f1cb96f8bf83f77
khabinov <oleg.khabinov@gmail.com>
2013-07-06 00:57:55 +0400

    Solution to second problem

--- src/project_euler_clj/core.clj
+++ src/project_euler_clj/core.clj
@@ -5,3 +5,12 @@
        (filter #(or (= 0 (rem % 3))
                     (= 0 (rem % 5))))
        (reduce +)))
+
+(defn fib [a b]
+  (cons a (lazy-seq (fib b (+ b a)))))
+
+(defn problem2 []
+  (->> (fib 1 2)
+       (take-while #(> 4000000 %))
+       (filter even?)
+       (reduce +)))
--- test/project_euler_clj/core_test.clj
+++ test/project_euler_clj/core_test.clj
@@ -4,3 +4,6 @@
 
 (deftest problem1-test
   (is (= 233168 (problem1))))
+
+(deftest problem2-test
+  (is (= 4613732 (problem2))))
